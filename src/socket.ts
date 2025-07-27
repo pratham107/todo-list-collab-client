10047036fdb77846c26b5ca74599a44a6d86a529
prathmesh <pbhopale107@gmail.com>
2025-07-28 01:04:55 +0530

    Final Commit

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://todo-list-collab-server.onrender.com"); // same as backend
+const socket = io("https://todo-list-collab-server.onrender.com");
 export default socket;
